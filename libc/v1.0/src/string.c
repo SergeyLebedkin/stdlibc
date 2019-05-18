85a89bca5f074582ffc2b80be737d1a61a048009
Sergii Liebodkin <s.lebedkin@gmail.com>
2019-05-18 16:57:15 +0300

    string.c prototipes

--- libc/v1.0/src/string.c
+++ libc/v1.0/src/string.c
@@ -1,1 +1,163 @@
 #include <string.h>
+
+// locate character in block of memory
+const void *memchr(const void *ptr, int value, size_t num)
+{
+    return NULL;
+}
+
+// locate character in block of memory
+void *memchr(void *ptr, int value, size_t num)
+{
+    return NULL;
+}
+
+// compare two blocks of memory
+int memcmp(const void *ptr1, const void *ptr2, size_t num)
+{
+    return 0;
+}
+
+// copy block of memory
+void *memcpy(void *destination, const void *source, size_t num)
+{
+    return NULL;
+}
+
+// move block of memory
+void *memmove(void *destination, const void *source, size_t num)
+{
+    return NULL;
+}
+
+// fill block of memory
+void *memset(void *ptr, int value, size_t num)
+{
+    return NULL;
+}
+
+// concatenate strings
+char *strcat(char *destination, const char *source)
+{
+    return NULL;
+}
+
+// locate first occurrence of character in string
+const char *strchr(const char *str, int character)
+{
+    return NULL;
+}
+
+// locate first occurrence of character in string
+char *strchr(char *str, int character)
+{
+    return NULL;
+}
+
+// compare two strings
+int strcmp(const char *str1, const char *str2)
+{
+    return 0;
+}
+
+// compare two strings using locale
+int strcoll(const char *str1, const char *str2)
+{
+    return 0;
+}
+
+// copy string
+char *strcpy(char *destination, const char *source)
+{
+    return NULL;
+}
+
+// get span until character in string
+size_t strcspn(const char *str1, const char *str2)
+{
+    return 0;
+}
+
+// get pointer to error message string
+char *strerror(int errnum)
+{
+    return NULL;
+}
+
+// get string length
+size_t strlen(const char *str)
+{
+    return 0;
+}
+
+// append characters from string
+char *strncat(char *destination, const char *source, size_t num)
+{
+    return NULL;
+}
+
+// compare characters of two strings
+int strncmp(const char *str1, const char *str2, size_t num)
+{
+    return 0;
+}
+
+// copy characters from string
+char *strncpy(char *destination, const char *source, size_t num)
+{
+    return NULL;
+}
+
+// locate characters in string
+const char *strpbrk(const char *str1, const char *str2)
+{
+    return NULL;
+}
+
+// locate characters in string
+char *strpbrk(char *str1, const char *str2)
+{
+    return NULL;
+}
+
+// locate last occurrence of character in string
+const char *strrchr(const char *str, int character)
+{
+    return NULL;
+}
+
+// locate last occurrence of character in string
+char *strrchr(char *str, int character)
+{
+    return NULL;
+}
+
+// get span of character set in string
+size_t strspn(const char *str1, const char *str2)
+{
+    return 0;
+}
+
+// locate substring
+const char *strstr(const char *str1, const char *str2)
+{
+    return NULL;
+}
+
+// locate substring
+char *strstr(char *str1, const char *str2)
+{
+    return NULL;
+}
+
+// split string into tokens
+char *strtok(char *str, const char *delimiters)
+{
+    return NULL;
+}
+
+// split string into tokens
+size_t strxfrm(char *destination, const char *source, size_t num)
+{
+    return 0;
+}
